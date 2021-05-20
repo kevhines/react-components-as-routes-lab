812359cd3b938a90aaeb2dd7b1e7254d1e6c1006
Kevin <kevhines@gmail.com>
2021-05-20 11:40:35 -0400

    commit tree 34602ebcbc3268277f9e779bd34bd654c2fc888b

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { actors } from '../data';
 
+
+
+function listActors() {
+  return actors.map(actor => {return <div>{actor.name}<ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul></div>})
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {listActors()}    
     </div>
   );
 };
